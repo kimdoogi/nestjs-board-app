18e126c6736490a1653b48451b1ff2860d36490a
김두기 <dugi_kim@tmax.co.kr>
2024-10-03 11:21:13 +0900

    validation logic add2

--- src/boards/pipes/board-status-validation.pipe.ts
+++ src/boards/pipes/board-status-validation.pipe.ts
@@ -1,9 +1,24 @@
-import { ArgumentMetadata, PipeTransform } from "@nestjs/common";
+import { ArgumentMetadata, BadRequestException, PipeTransform } from "@nestjs/common";
+import { BoardStatus } from "../boards.model";
 
 export class BoardStatusValidationPipe implements PipeTransform{
+
+    readonly StatusOptions = [
+            BoardStatus.PRIVATE,
+            BoardStatus.PUBLIC
+    ]
     transform(value: any, metadata: ArgumentMetadata) {
         console.log('value' , value);
         console.log('metadata', metadata);
+
+        if(!this.isStatusValid(value)){
+            throw new BadRequestException(`${value} is not in the status option`);
+        }
         return value;
+    }
+
+    private isStatusValid(status: any){
+        const index = this.StatusOptions.indexOf(status);
+        return index !== -1;
     }
 }
